96c54966c2fbd5547d3ce2aa8f0c75dfc5e26fd9
Vinayshrivastav <vinayshrivastav454@gmail.com>
2026-06-23 04:32:20 +0530

    Time: 0 ms (100%), Space: 47.5 MB (31.95%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xorSum = nums.length;
+        for(int i = 0; i<nums.length; i++){
+            xorSum ^= i^nums[i] ;
+        }
+        return xorSum;
+        
+    }
+}
