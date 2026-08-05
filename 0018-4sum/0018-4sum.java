71d8bced12a05aac2c67959d63bb55757b571bbf
Vinayshrivastav <vinayshrivastav454@gmail.com>
2026-08-06 02:04:56 +0530

    Time: 19 ms (83.66%), Space: 45.7 MB (63.64%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,53 +1,43 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Solution {
+class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        if (nums == null || nums.length < 4) {
-            return result;
-        }
-        
+        List<List<Integer>> arr = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-        
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
-            if ((long) nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) continue;
-            
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                
-                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
-                if ((long) nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target) continue;
-                
-                int left = j + 1;
-                int right = n - 1;
-                
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-                    
-                    if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-                        
-                        left++;
-                        right--;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
+        for(int i = 0; i < n-3; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            for(int j = i+1; j < n-2; j++){
+                if( j > i+1 && nums[j] == nums[j-1]){
+                continue;
                 }
+                int l = j+1;
+                int r = n-1;
+                
+                while(l < r){
+                    long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
+                    if(sum == target){
+                        arr.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        l++;
+                        r--;
+                        while(l < r && nums[l] == nums[l-1]){
+                            l++;
+                        }
+                        while(l < r && nums[r] == nums[r+1]){
+                            r--;
+                        }
+                    }
+                    else if(sum < target){
+                        l++;
+                    }
+                    else{
+                        r--;
+                    }
+
+                }
+
             }
         }
-        
-        return result;
+        return arr;
     }
 }
